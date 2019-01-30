046140394eef6b1fef2f4220d884e3787a34bae4
KV-Systems <development@kv-sys.ru>
2019-01-30 10:08:28 +0500

    Person class added

--- src/Person.php
+++ src/Person.php
@@ -1,7 +1,55 @@
 <?php
+namespace TTConsulting\Example;
+
+use TTConsulting\Example\Animal\GenericAnimal;
+
 /**
- * Created by PhpStorm.
- * User: Kraft
- * Date: 30.01.2019
- * Time: 9:31
+ * Class Person.
+ * @package TTConsulting\Example
  */
+class Person {
+
+    /**
+     * Cage for animals.
+     * @var $_cage array
+     */
+    private $_cage = [];
+
+    /**
+     * Cage volume.
+     * @var $_volume int
+     */
+    private $_volume;
+
+    /**
+     * Person constructor.
+     * Specifies the cell volume of a new person.
+     */
+    public function __construct(int $volume = 1)   {
+        $this->_volume = $volume;
+    }
+
+    /**
+     * Starts the animal in a cage.
+     * @param GenericAnimal $animal
+     * @return bool
+     */
+    public function take(GenericAnimal $animal) : bool  {
+        if($this->count() < $this->_volume) {
+            $this->_cage[] = $animal;
+            $response = true;
+        } else {
+            $response = false;
+        }
+        return $response;
+    }
+
+    /**
+     * Counts animals in a cage.
+     * @return int
+     */
+    public function count() : int {
+        return count($this->_cage);
+    }
+
+}
