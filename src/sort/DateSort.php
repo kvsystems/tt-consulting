f81bb0655cdd33ee45170a9a6d652d3c3aa01abd
KV-Systems <development@kv-sys.ru>
2019-01-30 14:12:10 +0500

    date sort added

--- src/sort/DateSort.php
+++ src/sort/DateSort.php
@@ -8,7 +8,12 @@ namespace TTConsulting\Example\Sort;
 class DateSort extends AGenericSort {
 
     public function sort(array $cage) : array {
-
+        uasort($cage,function($objA, $objB){
+            if($objA->entered() < $objB->entered()) return -1;
+            elseif($objA->entered() > $objB->entered()) return 1;
+            else return 0;
+        });
+        return $cage;
     }
 
 }
