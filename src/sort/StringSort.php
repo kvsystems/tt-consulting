e1148402ed3a5fa29784526f8193252517787ce0
KV-Systems <development@kv-sys.ru>
2019-01-30 13:20:33 +0500

    sort method for string sorting added

--- src/sort/StringSort.php
+++ src/sort/StringSort.php
@@ -8,7 +8,12 @@ namespace TTConsulting\Example\Sort;
 class StringSort extends AGenericSort {
 
     public function sort(array $cage) : array {
-
+        uasort($cage,function($objA, $objB){
+            if($objA->nickname() < $objB->nickname()) return -1;
+            elseif($objA->nickname() > $objB->nickname()) return 1;
+            else return 0;
+        });
+        return $cage;
     }
 
 }
