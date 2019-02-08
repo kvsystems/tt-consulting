ae9e0bbf4f3d4f16ab8db72720a85cc092f4c5b4
KV-Systems <development@kv-sys.ru>
2019-02-08 12:10:47 +0500

    updated sort algorithm

--- src/sort/TimeSort.php
+++ src/sort/TimeSort.php
@@ -7,7 +7,39 @@ namespace TTConsulting\Example\Sort;
  */
 class TimeSort extends AGenericSort {
 
+    /**
+     * Type of sorting by time.
+     * @var string
+     */
+    private $_type;
+
+    /**
+     * TimeSort constructor.
+     * Sets min, max, all time sorts.
+     * @param array $params
+     */
+    public function __construct(array $params = [])   {
+        if(isset($params[0])) {
+            $this->_type = __NAMESPACE__ . '\\' . 'Middleware\\' . $params[0];
+        }
+    }
+
+    /**
+     * Sort by time.
+     * @param array $cage
+     * @return array
+     */
     public function sort(array $cage) : array {
+        if(!is_null($this->_type)) {
+            $class = new $this->_type();
+            $cage = $class->filter($cage);
+        } else {
+            uasort($cage,function($objA, $objB){
+                if($objA->entered() < $objB->entered()) return -1;
+                elseif($objA->entered() > $objB->entered()) return 1;
+                else return 0;
+            });
+        }
         return array_values($cage);
     }
 
