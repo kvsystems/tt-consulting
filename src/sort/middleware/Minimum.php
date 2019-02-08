ae85b5915dbe0d7c9275a66bd2e7d5dc6fa2927e
KV-Systems <development@kv-sys.ru>
2019-02-08 13:11:40 +0500

    updated algorithm

--- src/sort/middleware/Minimum.php
+++ src/sort/middleware/Minimum.php
@@ -10,8 +10,9 @@ class Minimum implements IMiddleware {
 
     public function filter(array $cage = []) : array    {
         $min = 0; $result = [];
+        if(isset($cage[0])) $min = $cage[0]->entered();
         foreach($cage as $animal)
-            if($min > $animal->entered()) $temp = $animal;
+            if($min >= $animal->entered()) $temp = $animal;
         if(isset($temp)) $result[] = $temp;
         return $result;
     }
